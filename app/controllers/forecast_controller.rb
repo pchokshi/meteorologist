e7dac28c4228f4cc496e5bb030fc67179a031227
pchokshi <poojachokshi@wireless-128-135-241-199.uchicago.edu>
2015-10-14 16:32:20 -0500

    done with coordinate to weather

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -20,16 +20,19 @@ class ForecastController < ApplicationController
     parsed_data = JSON.parse(open(url).read)
     current_temp = parsed_data["currently"]["temperature"]
     current_summary = parsed_data["currently"]["summary"]
+    summary_sixty = parsed_data["minutely"]["summary"]
+    summary_hours = parsed_data["daily"]["data"][0]["summary"]
+    summary_days = parsed_data["daily"]["summary"]
 
     @current_temperature = current_temp
 
     @current_summary = current_summary
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = summary_sixty
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = summary_hours
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = summary_days
 
     render("coords_to_weather.html.erb")
   end
